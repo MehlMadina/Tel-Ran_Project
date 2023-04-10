cfac04378158005aed567668ff748730e220a261
Madina Mel <wolfi251199@gmail.com>
2023-04-10 15:18:19 +0200

    Added Banner and Footer components

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
+import s from "./style.module.css";
+import banner from "../../media/banner.png";
 
 export default function Banner() {
   return (
-    <div>Banner</div>
-  )
+    <div className={s.banner_section}>
+      <div className={s.banner_sub_section}>
+        <div className={s.info}>
+          <p className={s.text_sale}>Sale</p>
+          <p className={s.text_descr}>New season</p>
+          <div>
+            <button>Sale</button>
+          </div>
+        </div>
+        <img src={banner} alt="banner_img" />
+      </div>
+    </div>
+  );
 }
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
-import s from './style.module.css';
+import React from "react";
+import s from "./style.module.css";
+import { InstagramOutlined } from "@ant-design/icons";
+import { WhatsAppOutlined } from "@ant-design/icons";
 
 export default function Footer() {
   return (
     <div className={s.footer}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero asperiores tempore minus soluta deserunt blanditiis reprehenderit atque veniam fugiat pariatur? Exercitationem, quia impedit ducimus fuga, totam nemo praesentium explicabo laborum accusantium quos voluptatum culpa. Sequi laborum dolores similique, non autem voluptate necessitatibus magnam assumenda fugiat quas doloribus at cumque unde.
+      <div className={s.contacts}>
+        <h1>Contacts</h1>
+        <p>+49 999 999 99 99</p>
+        <div className={s.social_media}>
+          <a href="#">
+            <WhatsAppOutlined className={s.icon} />
+            WhatsApp
+          </a>
+          <a href="https://www.instagram.com/">
+            <InstagramOutlined className={s.icon} />
+            instagram
+          </a>
+        </div>
+      </div>
+      <div className={s.address}>
+        <h1>Address</h1>
+        <h2>Linkstraße 2, 8 OG, 10785, Berlin, Deutschland</h2>
+        <p>Working Hours:</p>
+        <h3> 24 hours a day </h3>
+      </div>
     </div>
-  )
+  );
 }
