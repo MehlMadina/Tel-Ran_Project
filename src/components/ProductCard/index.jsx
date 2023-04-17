fa433b9f891360d3646d6400260b2e77a8750d6c
Madina Mel <wolfi251199@gmail.com>
2023-04-17 18:30:50 +0200

    Added some styles and сalculated the discount percentage for ProductCard

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -2,16 +2,30 @@ import React from "react";
 import s from "./style.module.css";
 import { Link } from "react-router-dom";
 
-export default function ProductCard({ id, title, image, price }) {
+
+export default function ProductCard({ id, title, image, price, discont_price }) {
   const src_img = `http://localhost:3333${image}`;
+
   const link = `/product/${id}`;
+
+  const sale_percentage = Math.round(((price - discont_price) / price) * 100);
 
   return (
     <Link to={link}>
       <div className={s.product_item}>
         <img src={src_img} alt={title} />
-        <p>{price}$</p>
-        <p>{title}</p>
+        <div className={s.price_info}>
+          {discont_price === null ? (
+           <p className={s.main_price}>{price}$</p>
+          ) : (
+            <>
+              <p className={s.discount_price}>{discont_price}$</p>
+              <p className={s.price}>{price}$</p>
+              <p className={s.sale_percentage}>{sale_percentage}%</p>
+            </>
+          )}
+        </div>
+        <p className={s.title}>{title}</p>
       </div>
     </Link>
   );
--- src/components/ProductItem/index.jsx
+++ src/components/ProductItem/index.jsx
@@ -2,17 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 import s from './style.module.css';
 
-export default function ProductItem({ id, title, image, price }) {
+export default function ProductItem({ id, title, image, price, discont_price }) {
   
   const product_image = `http://localhost:3333/product_img/${id}.jpeg`;
+
+  const link = `/product/${id}`;
+
+  const sale_percentage = Math.round(((price - discont_price) / price) * 100);
   
   return (
-    <div className={s.product_item}>
-      <Link>
+    <Link to={link}>
+       <div className={s.product_item}>
         <img src={product_image} alt={title} />
-        <p>{price}$</p>
-        <p>{title}</p>
-      </Link>
-    </div>
+        <div className={s.price_info}>
+          {discont_price === null ? (
+           <p className={s.main_price}>{price}$</p>
+          ) : (
+            <>
+              <p className={s.discount_price}>{discont_price}$</p>
+              <p className={s.price}>{price}$</p>
+              <p className={s.sale_percentage}>{sale_percentage}%</p>
+            </>
+          )}
+        </div>
+        <p className={s.title}>{title}</p>
+      </div>
+    </Link>
   );
 }
