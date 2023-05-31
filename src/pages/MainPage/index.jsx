382a2de2084747426f6f8cf35333bed4ce8554ae
Madina Mel <wolfi251199@gmail.com>
2023-05-31 10:09:04 +0200

    Added some changes

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,14 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Banner from "../../components/Banner";
 import CategoriesContainer from "../../components/CategoriesContainer";
 import DiscountForm from "../../components/DiscountForm";
 import SalesContainer from "../../components/SalesContainer";
 
 export default function MainPage() {
-
-  useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  }, []);
 
   return (
     <div>
